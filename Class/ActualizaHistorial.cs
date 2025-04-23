d033220d6734fdebe690e90f83a2e420f4b02b18
JoseMRT2004 <josemtaverasreyes@gmail.com>
2025-04-23 00:44:49 -0400

    refactor: clean up whitespace and add TODO comments for data update logic in SqlDbActualizarHistorial and FileTxtActualizarHistorial classes

--- Class/ActualizaHistorial.cs
+++ Class/ActualizaHistorial.cs
@@ -5,23 +5,16 @@ namespace Mundo_Morse
 
     public class SqlDbActualizarHistorial : IActualizaDato
     {
-
-
+        // TODO: Aquí va la lógica para actualizar los datos del modo carrera y desafío  en la base de datos
         public void ActualizaDatoModoCarrera() { Console.WriteLine("Ejecutando la función [ TXT ] : GuardaModoDesafio"); }
         public void ActualizaDatoModoDesafio() { Console.WriteLine("Ejecutando la función [ TXT ] : GuardaModoDesafio"); }
-
     }
-
-
 
     public class FileTxtActualizarHistorial : IActualizaDato
     {
-
-
+        // TODO: Aquí va la lógica para actualizar los datos del modo carrera y desafío  en un archivo TXT
         public void ActualizaDatoModoCarrera() { Console.WriteLine("Ejecutando la función [ TXT ] : GuardaModoDesafio"); }
         public void ActualizaDatoModoDesafio() { Console.WriteLine("Ejecutando la función [ TXT ] : GuardaModoDesafio"); }
     }
-
-
 
 }
